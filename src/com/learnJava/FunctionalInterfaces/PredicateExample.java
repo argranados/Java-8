6fed99448826b3c84440e463fc29d22b62ceb8b6
Alfonso Granados - vn540b5 <Alfonso.Granados@walmart.com>
2023-02-17 20:16:46 -0600

    Predicate Lab

---
+++ src/com/learnJava/FunctionalInterfaces/PredicateExample.java
@@ -0,0 +1,31 @@
+package com.learnJava.FunctionalInterfaces;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+        static Predicate<Integer> p = (i)->{ return i%2 ==0; };
+        static Predicate<Integer> p1 = i -> i%2 ==0;
+        static Predicate<Integer> p2 = i -> i%5 ==0;
+
+        public static void predicateAnd(){
+            System.out.println("Predicate AND result is: " +p1.and(p2).test(10));
+            System.out.println("Predicate AND result is: " +p1.and(p2).test(9));
+        }
+
+    public static void predicateOr(){
+        System.out.println("Predicate OR result is: " +p1.or(p2).test(10));
+        System.out.println("Predicate OR result is: " +p1.or(p2).test(8));
+    }
+
+    public static void predicateNegate(){
+        System.out.println("Predicate NEGATE result is: " +p1.or(p2).negate().test(10));
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(p.test(4));
+//        System.out.println(p.test(3));
+        predicateAnd();
+        predicateOr();
+        predicateNegate();
+    }
+}
